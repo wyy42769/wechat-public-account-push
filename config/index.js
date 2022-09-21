33703bd842581305b85413d0779d93f81c566541
wyy42769 <104123459+wyy42769@users.noreply.github.com>
2022-09-21 13:33:30 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -139,7 +139,7 @@ export const config = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "",
+  CALLBACK_TEMPLATE_ID: "9avLlW-Arw92P4WPQTqqBjiHv1SXNgpt919LJUlJKBM",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
